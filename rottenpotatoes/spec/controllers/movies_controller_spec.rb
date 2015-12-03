a604c5631480f8caec3ca59dcff115b21ce8765c
Darwin <darwin@centralarticle.com>
2015-12-03 09:26:55 -0500

    Working with 80% grade

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -2,6 +2,62 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe MoviesController do
+  describe 'list movies' do
+    context 'order by title' do
+      it 'should list the movies ordered by title' do
+        movies = [double('Amelie'),double('Starwars')]
+        sort = 'title'
+        ordering = {title: :asc}
+        selected_ratings = {rating: ["G", "NC-17", "PG", "PG-13", "R"]}
+        #expect(Movie).to receive(:where).with(selected_ratings).and_return(movies)
+        #expect(Movie).to receive(:order).with(ordering).and_return(movies)
+      end
+    end
+    
+    context 'order by release date' do
+      it 'should list the movies ordered by release date' do
+      end
+    end
+  end
+  describe 'create movie' do
+    it 'creates a movie and redirect to movies page' do
+      movie_params = {title: 'Pi', rating: 'PG-13', release_date: '1999-06-02' }
+      movie = double('Pi')
+      movie.stub(:title).and_return(movie_params[:title])
+      expect(Movie).to receive(:create!).with(movie_params).and_return(movie)
+      post :create, {movie: movie_params}
+      expect(flash[:notice]).to eq "#{movie.title} was successfully created."
+      expect(response).to redirect_to(movies_path)
+    end
+  end
+
+  describe 'update movie' do
+    it 'updates a movie and redirect to the movie page' do
+      id = "1"
+      movie_params = {title: 'Pi', rating: 'PG-13', release_date: '1999-06-02' }
+      movie = double('Pi')
+      movie.stub(:title).and_return(movie_params[:title])
+      expect(Movie).to receive(:find).with(id) { movie }
+      expect(movie).to receive(:update_attributes!).with(movie_params).and_return(movie)
+      patch :update, id: id, movie: movie_params
+      expect(flash[:notice]).to eq "#{movie.title} was successfully updated."
+      expect(response).to redirect_to(movie_path(movie))
+    end
+  end
+
+  describe 'destroy movie' do
+    it 'destroys a movie and redirects to the movies page' do
+      id = '1'
+      movie = double('Pi')
+      movie.stub(:title).and_return('Pi')
+      expect(Movie).to receive(:find).with(id) {movie}
+      expect(movie).to receive(:destroy).and_return(movie)
+      delete :destroy, id: id
+      expect(flash[:notice]).to eq "Movie '#{movie.title}' deleted."
+      expect(response).to redirect_to(movies_path)
+    end
+  end
+
   describe 'find with same director' do
     before :each do
       @fake_movies = [double('Star Wars'), double('THX-1138')]
@@ -39,6 +95,16 @@ describe MoviesController do
     end
 
     context 'the movie does not have director information' do
+      it 'should redirect to movies when there is no director information' do
+        movie = double('movie without director')
+        movie.stub(:id).and_return('2')
+        movie.stub(:title).and_return('Movie without director')
+        movie.stub(:director).and_return('')
+        expect(Movie).to receive(:find).with(movie.id) { movie }
+        allow(movie).to receive(:find_movies_with_same_director) { [] }
+        get :find_with_same_director, {:id => movie.id}
+        expect(response).to redirect_to(movies_path)
+      end
     end
   end
 end
